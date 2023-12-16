b2c28ef7c5d7c3bd1e061263f4773012746be104
omer8990 <omer.haimovitz@gmail.com>
2023-12-16 10:01:53 +0200

    added gitignore and the super basic version of the api

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import os
 
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel('gemini-pro')
+
+chat = model.start_chat()
+response = chat.send_message('Hello, what should I have for dinner?')
+print(response.text)  # 'Here are some suggestions...'
+response = chat.send_message("How do I cook the first one?")
